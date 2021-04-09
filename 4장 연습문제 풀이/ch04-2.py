1de4a2978cd6a6dbbc8cfc584f61e05974248d89
yuuuzzzin <uiuiui118437@gmail.com>
2021-04-09 16:22:18 +0900

    add ch04-2.py

--- 4장 연습문제 풀이/ch04-2.py
+++ 4장 연습문제 풀이/ch04-2.py
@@ -1,6 +1,8 @@
-a = 1
-def vartest(a):
-    a = a + 1
+def average(*args):  # 입력으로 들어오는 수의 개수가 정해져 있지 않기 때문에 *args 사용
+    sum = 0
+    for i in args:
+        sum += i
+    average = sum / len(args) # 수의 총 합을 입력으로 받은 수의 개수로 나누어 평균 구하기
+    return average
 
-vartest(a)
-printa(a)
+print(average(1, 2, 3, 4, 5))
