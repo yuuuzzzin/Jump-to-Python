bedfdfaf726638f9f2b97da09e0d986b87a02387
yuuuzzzin <uiuiui118437@gmail.com>
2021-04-09 16:22:24 +0900

    add ch04-3.py

--- 4장 연습문제 풀이/ch04-3.py
+++ 4장 연습문제 풀이/ch04-3.py
@@ -1,6 +1,5 @@
-a = 1
-def vartest(a):
-    a = a + 1
+input1 = input("첫번째 숫자를 입력하세요:")
+input2 = input("두번째 숫자를 입력하세요:")
 
-vartest(a)
-printa(a)
+total = int(input1) + int(input2)  # int 함수를 사용해 문자열을 정수형으로 바꾸어 줌
+print("두 수의 합은 %s 입니다" % total)
