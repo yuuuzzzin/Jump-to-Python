52edf97142e9fcef6bb20beb91adedb5197a5624
yuuuzzzin <uiuiui118437@gmail.com>
2021-04-13 20:10:23 +0900

    add ch08-14.py

---
+++ 8장 종합문제 풀이/ch08-14.py
@@ -0,0 +1,17 @@
+words = input("입력: ")
+result = ""
+temp = ""
+count = 0
+
+result += words[0]
+
+for word in words:
+    if word:
+        temp = word
+        if count:
+            
+
+    
+    result+=word
+    
+print(result)
