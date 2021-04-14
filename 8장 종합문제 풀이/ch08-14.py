ce6fe6fbdf934ffdc278930832d695da6f53cae8
yuuuzzzin <uiuiui118437@gmail.com>
2021-04-14 18:20:45 +0900

    add ch08-14.py

--- 8장 종합문제 풀이/ch08-14.py
+++ 8장 종합문제 풀이/ch08-14.py
@@ -1,17 +1,20 @@
+def compress(s):
+    temp = ""
+    cnt = 0
+    result = ""
+
+    for c in s:
+        if c != temp:
+            temp = c
+            if cnt:
+                result += str(cnt)
+            result += c
+            cnt = 1
+        else:
+            cnt += 1
+    result += str(cnt)
+    return result
+
+
 words = input("입력: ")
-result = ""
-temp = ""
-count = 0
-
-result += words[0]
-
-for word in words:
-    if word:
-        temp = word
-        if count:
-            
-
-    
-    result+=word
-    
-print(result)
+print("출력: %s" % compress(words))
