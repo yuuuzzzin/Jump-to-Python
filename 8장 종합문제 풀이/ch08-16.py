207744b9c87153a5ac12d14368578a80cb148876
yuuuzzzin <uiuiui118437@gmail.com>
2021-04-14 18:21:03 +0900

    mod ch08-16.py

---
+++ 8장 종합문제 풀이/ch08-16.py
@@ -0,0 +1,20 @@
+dic = {
+    '.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E', '..-.': 'F',
+    '--.': 'G', '....': 'H', '..': 'I', '.---': 'J', '-.-': 'K', '.-..': 'L',
+    '--': 'M', '-.': 'N', '---': 'O', '.--.': 'P', '--.-': 'Q', '.-.': 'R',
+    '...': 'S', '-': 'T', '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X',
+    '-.--': 'Y', '--..': 'Z'
+}
+
+
+def morse(m):
+    result = []
+    for word in m.split("  "):
+        for char in word.split(" "):
+            result.append(dic[char])
+        result.append(" ")
+    return "".join(result)
+
+
+a = input()
+print(morse(a))
